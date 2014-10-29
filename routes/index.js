bddde019bb2e6bba1eec6bb65abf12b23851b41d
tailsx <davidof.chen@mail.utoronto.ca>
2014-10-29 01:07:16 -0400

    got sign up working

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,8 @@ var token; //For now, store token in a variable for later use
 /* GET home page. */
 router.get('/', function(req, res) {
   var locals = {
-        title: 'This is my sample app',
         url: gapi.url,
-        test: 'http://localhost:3000/test'
+        signup: '/signup'
       };
   res.render('index.jade', locals);
 });
@@ -47,10 +46,9 @@ router.get('/oauth2callback', function(req, res) {
         url: gapi.url,
         test: 'http://localhost:3000/test'
       };
-  res.render('index.jade', locals);
+  res.redirect('/users/main');
 });
 
-/* */
 router.get('/code', function(req, res) {
   var locals = {
         title: 'This is my CODE app',
@@ -60,6 +58,45 @@ router.get('/code', function(req, res) {
   res.render('index.jade', locals);
 });
 
+router.get('/signup', function(req, res){
+    var locals = {
+        title: 'This is my CODE app',
+        url: gapi.url,
+        signup: '/signup'
+      };
+  res.render('signup.jade', locals);
+});
+
+router.post('/signup', function(req, res){
+  console.log(req.body);
+  var test ='https://inaturalist.org/users.json?user[email]=' + req.body.email
+                                          + '&user[login]=' + req.body.username
+                                          + '&user[password]=' + req.body.password
+                                          + '&user[password_confirmation]=' + req.body.confirmpw
+  console.log(test);
+  var r = request.post({
+    url: test
+  }, function(err, res, body){
+      console.log(body);
+  })
+  res.redirect('users/main');
+});
+
+router.get('/logout', function(req, res){
+  var r = request.get({
+    url: 'https://inaturalist.org/logout',
+    headers: { 'Authorization': 'Bearer ' + token }
+    }, function(err, res, body){
+      console.log();
+    });
+  var locals = {
+        title: 'This is my TEST app',
+        url: gapi.url,
+        test: 'http://localhost:3000/test'
+      };
+  token=undefined;
+  res.redirect('/');
+});
 
 /* example in calling app */
 function requestCallback(err, res, body) {
@@ -67,7 +104,7 @@ function requestCallback(err, res, body) {
 }
 
 router.get('/test', function(req, res) {
-  var r = request.post({
+  var r = request.get({
     url: 'https://inaturalist.org/users/edit.json',
     headers: { 'Authorization': 'Bearer ' + token }
     }, requestCallback);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var gapi = require('../lib/gapi');
+var request = require('request');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource');
+});
+
+router.get('/main', function(req, res) {
+  var locals = {
+        title: 'This is my CODE app',
+        url: gapi.url,
+        logout: '/logout'
+      };
+  res.render('main.jade', locals);
 });
 
 module.exports = router;
