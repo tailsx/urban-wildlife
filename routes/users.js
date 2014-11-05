3997c1ce89bc18f5e90dba2b8f5ac2bfcc43a26a
tailsx <davidof.chen@mail.utoronto.ca>
2014-11-05 01:39:38 -0500

    Fix views

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ var gapi = require('../lib/gapi');
 var request = require('request');
 var index = require('../routes/index');
 var Client = require('node-rest-client').Client;
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -11,30 +13,57 @@ router.get('/', function(req, res) {
 });
 
 router.get('/main', function(req, res) {
-  var locals = {
-        profile: '/users/profile',
-        info: 'hello',
-        logout: '/logout'
-      };
-  console.log(req.params)
-  res.render('main.jade', locals);
+	client = new Client();
+
+	var base = 'https://inaturalist.org';
+	var endpoint = '/users/edit';
+	var url = base + endpoint + '.json';
+
+	var args = {
+        headers:{"Content-Type": "application/json",
+    			"Authorization": 'Bearer ' + global.token}
+    }
+
+	// direct way
+	client.get(url, args, function(data, response){
+	    var parsed = JSON.parse(data);
+		res.render('main.jade',{ profile: '/users/profile',
+		  						 info: parsed.login,
+		  						 logout: '/logout',
+		  						 toRecords : '/users/records/'+ parsed.login});
+	});
 });
 
 router.get('/profile', function(req,res){
-	console.log("hello" + index.token);
-	console.log("hello2" + global.token);
-	var profile = 'https://inaturalist.org/users/edit.json';
-	var r = request.get({
-	    url: profile,
-	    headers: { 'Authorization': 'Bearer ' + global.token }
-	  }, function(err, response, body){
-	      console.log(body);
-	      var parsedBody = JSON.parse(body);
-	      console.log(parsedBody.login);
-	      res.render('main.jade',{ profile: '/users/profile',
-	  							   info: 'Username: ' + parsedBody.login,
-	  							   logout: '/logout'});
-	  });
+	client = new Client();
+
+	var base = 'https://inaturalist.org';
+	var endpoint = '/users/edit';
+	var url = base + endpoint + '.json';
+
+	var args = {
+        headers:{"Content-Type": "application/json",
+    			"Authorization": 'Bearer ' + global.token}
+    }
+
+    // direct way
+	client.get(url,args, function(data, response){
+	            // parsed response body as js object
+	            //console.log(data);
+	            var parsed = JSON.parse(data);
+	      		console.log(parsed);
+	      		//console.log(parsed[1].photos[0].medium_url);
+
+	            // raw response
+	            //console.log(response);
+	res.render('profile.jade',{ profile: '/users/profile',
+		  						info: parsed.login,
+		  						logout: '/logout',
+		  						creation: parsed.created_at,
+		  						count: parsed.observations_count,
+		  						email: parsed.email,
+		  						toRecords : '/users/records/'+parsed.login});
+	});
 });
 
 router.get('/records/:username', function(req,res){
@@ -46,24 +75,15 @@ router.get('/records/:username', function(req,res){
 
 	// direct way
 	client.get(url, function(data, response){
-	            // parsed response body as js object
-	            //console.log(data);
-	            var parsed = JSON.parse(data);
-	      		//console.log(parsed);
-	      		//console.log(parsed[1].photos[0].medium_url);
-
-	            // raw response
-	            //console.log(response);
-	res.render('records.jade',{ profile: '/users/profile',
-	  						 info: 'Username: ',
-	  						 logout: '/logout',
-	  						 results: parsed});
+		var parsed = JSON.parse(data);
+		res.render('records.jade',{ profile: '/users/profile',
+			  						info: req.params.username,
+			  						logout: '/logout',
+			  						results: parsed});
 	});
 });
 
 router.get('/newrecord', function(req,res){
-	console.log("hello" + index.token);
-	console.log("hello2" + global.token);
 	var profile = 'https://inaturalist.org/users/edit.json';
 	var r = request.get({
 	    url: profile,
@@ -80,16 +100,17 @@ router.get('/newrecord', function(req,res){
 
 router.post('/newrecord', function(req,res){
 	client = new Client();
-	console.log(req.body.cname);
 
     var args = {
-        parameters:{'observation[species_guess]': req.body.cname},
+        parameters:{'observation[species_guess]': req.body.guess,
+    				'observation[observed_on_string]': req.body.seen,
+    				'observation[description]': req.body.desc},
         headers:{"Content-Type": "application/json",
     			"Authorization": 'Bearer ' + global.token}
     }
 
     client.post('https://inaturalist.org/observations.json',args, function(data,response){
-        res.send(data);
+        res.redirect('/users/main');
     });
 });
 
@@ -125,20 +146,36 @@ router.get('/playground', function(req,res){
 	  							   logout: '/logout'});
 });
 
-router.post('/playground', function(req,res){
-	res.send(req.body);
+router.post('/playground', multipartMiddleware, function(req,res){
+	console.log(req.body);
+	console.log(req.files);
+	//1055035
+
 /*	client = new Client();
 	console.log(req.body.cname);
 
     var args = {
-        parameters:{'observation[species_guess]': req.body.cname},
-        headers:{"Content-Type": "application/json",
-    			"Authorization": 'Bearer ' + global.token}
+        parameters:{'observation_photo[observation_id]': '1055033'},
+        headers:{'Content-Type': 'multipart/form-data',
+        		'Authorization': 'Bearer ' + global.token}
     }
 
-    client.post('https://inaturalist.org/observations.json',args, function(data,response){
+    client.post('https://inaturalist.org/observation_photos',args, function(data,response){
+        console.log(response);
+        console.log(data);
         res.send(data);
     });*/
+    var test = {
+    	url: 'https://inaturalist.org/observation_photos?observation_photo[observation_id]=1055033',
+    	formData:  req.files.file
+    }
+    request.post(test, function optionalCallback(err, httpResponse, body) {
+	  if (err) {
+	    return console.error('upload failed:', err);
+	  }
+	  console.log('Upload successful!  Server responded with:', body);
+	});
+
 });
 
 module.exports = router;
