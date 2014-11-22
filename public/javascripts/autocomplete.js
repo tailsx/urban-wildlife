899e4fe195ea2a583708335e0dbac40346df6c9f
tailsx <davidof.chen@mail.utoronto.ca>
2014-11-21 22:52:01 -0500

    Added autocomplete suggestions for new records

--- public/javascripts/autocomplete.js
+++ public/javascripts/autocomplete.js
@@ -1,7 +1,18 @@
+var getMatches = function (data) {
+    var cats = [];
+    data.forEach(function (item) {
+        item.category.forEach(function (it) {
+            // Note that in a final soluton you'd want to de-dupe the category items
+            cats.push(it);
+        });
+    });
+    return cats;
+};
+
 var nbaTeams = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('team'),
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('apiVersion'),
   queryTokenizer: Bloodhound.tokenizers.whitespace,
-  prefetch: '../data/nba.json'
+  remote: 'http://data.canadensys.net/vascan/api/0.1/search.json?q=%QUERY'
 });
  
 var nhlTeams = new Bloodhound({
@@ -9,16 +20,47 @@ var nhlTeams = new Bloodhound({
   queryTokenizer: Bloodhound.tokenizers.whitespace,
   prefetch: '../data/nhl.json'
 });
- 
+
+var species = new Bloodhound({
+    datumTokenizer: function (datum) {
+        return Bloodhound.tokenizers.whitespace(datum.value);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+        url: 'http://data.canadensys.net/vascan/api/0.1/search.json?q=%QUERY',
+        rateLimitWait : 600,
+        filter: function (species) {
+            // Map the remote source JSON array to a JavaScript array
+            return $.map(species.results, function (s) {
+                var test = [];
+                s.matches.forEach(function (it) {
+                    test.push({
+                        value: it.scientificName
+                    });
+                });
+                return test;
+            });
+        }
+    }
+});
+
 nbaTeams.initialize();
 nhlTeams.initialize();
+species.initialize();
  
-$('#multiple-datasets .typeahead').typeahead({
+$('#canadensys .typeahead').typeahead({
+  highlight: true
+}, {
+    displayKey: 'value',
+    source: species.ttAdapter()
+});
+
+/*$('#multiple-datasets .typeahead').typeahead({
   highlight: true
 },
 {
   name: 'nba-teams',
-  displayKey: 'team',
+  displayKey: 'apiVersion',
   source: nbaTeams.ttAdapter(),
   templates: {
     header: '<h3 class="league-name">NBA Teams</h3>'
@@ -31,4 +73,4 @@ $('#multiple-datasets .typeahead').typeahead({
   templates: {
     header: '<h3 class="league-name">NHL Teams</h3>'
   }
-});
+});*/
